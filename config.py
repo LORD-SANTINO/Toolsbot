b3548be57e4efdeac12e83f9985fe96b04c16680
LORD-SANTINO <mikkyrose297@gmail.com>
2026-05-19 04:04:06 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "8610612533:AAH_yEXPsVaCrWnh8zzT6eHfTkR3DKeDduo"
+ADMIN_ID = 7243305432
+DB_PATH = "tools.db"
+INVITE_POINTS = 3     # Default reward per invite (can be changed via /setinvitepoints)
