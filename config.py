1d352497e6e9b41fe19a4aace7f3290f4d58e10a
LORD-SANTINO <mikkyrose297@gmail.com>
2026-05-21 09:29:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ BOT_TOKEN = "8610612533:AAH_yEXPsVaCrWnh8zzT6eHfTkR3DKeDduo"
 ADMIN_ID = 7243305432
 DB_PATH = "tools.db"
 INVITE_POINTS = 3     # Default reward per invite (can be changed via /setinvitepoints)
+# config.py
+REQUIRED_CHANNELS = ["@Daxbots", "@firstdaxlord"]
